ed8f00b7cd02f445e25e278040cec3b4bfbd3f9e
Makson Costa <makimec@hotmail.com>
2023-03-11 15:09:21 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,7 +7,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		
 		Gato gato = new Gato();
-		Livros livros = new Livros();
+s		Livros livros = new Livros();
 		
 		System.out.println(gato);
 		System.out.println(livros);
